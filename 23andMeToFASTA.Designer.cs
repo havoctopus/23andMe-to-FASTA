023360957ff7b98124a0ba1c777127108764ce46
Felix Chandrakumar <i@fc.id.au>
2014-04-23 19:28:02 +0930

    Initial Commit

---
+++ 23andMeToFASTA.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace NS_23andMe_to_FASTA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open 23andMe File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save mtDNA FASTA File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "23andMe files|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "FASTA Files|*.fasta";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 124);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(201, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(38, 17);
+            this.statusLbl.Text = "Done.";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 146);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "23andMe to FASTA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLbl;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ 23andMeToFASTA.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NS_23andMe_to_FASTA
+{
+    public partial class Form1 : Form
+    {
+        string reference = NS_23andMe_to_FASTA.Properties.Resources.rCRS_ref;
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+        char[] fasta = null;
+        string filename = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                button2.Enabled = false;
+                statusLbl.Text = "Working...";
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if(filename!=null)
+            {
+                fasta = NS_23andMe_to_FASTA.Properties.Resources.rCRS_seq.ToCharArray();
+                string[] lines = File.ReadAllLines(filename);
+                string[] data = null;
+                string position = null;
+                foreach(string line in lines)
+                {
+                    if (line.StartsWith("#"))
+                        continue;
+                    data = line.Split(new char[] { '\t' });
+                    if(data[1] == "MT")
+                    {
+                        //
+                        if(dict.TryGetValue(data[0], out position))
+                        {
+                            if (data[3] != "-" && data[3] != "--")
+                            {
+                                fasta[int.Parse(position) - 1] = data[3].ToCharArray()[0];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button2.Enabled = true;
+            statusLbl.Text = "Done.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringReader reader = new StringReader(reference);
+            string line = null;
+            string[] data = null;
+            while ((line = reader.ReadLine()) != null)
+            {
+                data = line.Split(new char[] { ',' });
+                if(!dict.ContainsKey(data[0]))
+                    dict.Add(data[0], data[1]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(">"+Path.GetFileName(saveFileDialog1.FileName)+"\r\n");
+                int i = 1;
+                foreach(char c in fasta)
+                {
+                    sb.Append(c);
+                    if (i == 50)
+                    {
+                        i = 0;
+                        sb.Append("\r\n");
+                    }
+                    i++;
+                }
+                File.WriteAllText(saveFileDialog1.FileName, sb.ToString());
+                statusLbl.Text = "File saved.";
+            }
+        }
+    }
+}
